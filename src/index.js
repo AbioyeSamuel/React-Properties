1e212bb68f7457497e3c44d08760e64fa6738824
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-21 09:39:20 +0100

    styles of the texts changed to red.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 function Card(props) {
   return (
-    <div>
+    <div className="myStyle">
       <h2>{props.name}</h2>
       <img src={props.img} alt="avatar_img" />
       <p>{props.tel}</p>
